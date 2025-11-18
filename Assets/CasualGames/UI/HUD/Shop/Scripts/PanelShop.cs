3351bab397386e89329b4d4ddea384cc47ddcdda
ElliotDev <gametraxworks@gmail.com>
2025-11-18 03:00:49 -0600

    feat(settings) Add new UI
    Added new UI Layout settings

--- Assets/CasualGames/UI/HUD/Shop/Scripts/PanelShop.cs
+++ Assets/CasualGames/UI/HUD/Shop/Scripts/PanelShop.cs
@@ -28,16 +28,30 @@ public class PanelShop : MonoBehaviour
                 print("Shop deactivated");
             });
     }
-
+    
+    
+    public void EnterSetting()
+    {
+        fadeSystem.FadeInOut(fadeWaitTime,
+            onFadeInComplete: () =>
+            {
+                EventManager.TriggerEvent("OnOpenMenu",false);
+                EventManager.TriggerEvent("OnOpenSettings",true);
+                print("Settings activated");
+            });
+    }
 
     public void ExitSettings()
     {
-        fadeSystem.FadeInOut(fadeWaitTime, () =>
-        {
-            Debug.Log("Settings Exited");
-        });
+        fadeSystem.FadeInOut(fadeWaitTime,
+            onFadeInComplete: () =>
+            {
+                EventManager.TriggerEvent("OnOpenMenu",true);
+                EventManager.TriggerEvent("OnOpenSettings",false);
+                print("Settings deactivated");
+            });
     }
-
+    
    
     public void Activate_Page_Settings()
     {
